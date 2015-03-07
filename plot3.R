18e66b5850864f672f8d4888cdba6e3d6e511b42
Nathaniel Ng <nathanielng@users.noreply.github.com>
2015-03-07 11:10:29 +0800

    Add code for creating plots 1 to 4, as well as the png files for the plots

---
+++ plot3.R
@@ -0,0 +1,56 @@
+get.file <- function(dest, fileUrl) {
+  if (!file.exists(dest)) {
+    zipfile <- "household_power_consumption.zip"
+    if (!file.exists(zipfile)) {
+      download.file(fileUrl, zipfile, method="curl")
+    }
+    unzip(zipfile)
+    if (file.exists(dest)) {
+      rm(zipfile,fileUrl)
+    }
+  }
+}
+
+read.file.by.blocks <- function(file, rblock = 500000, rmax = 2100000) {
+  rstart <- 0
+  all.data <- data.frame()
+  has.header <- TRUE
+  data0 <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE,
+                      na.strings="?", skip = 0, nrow = 10)
+  while (rstart < rmax) {
+    data <- read.table(file, header=has.header, sep=";", stringsAsFactors=FALSE,
+                       na.strings="?", skip = rstart, nrow = rblock,
+                       colClasses = c(rep("character",2), rep("numeric",7)))
+    names(data) <- names(data0)
+    data <- data[(data[,1] == "1/2/2007" | data[,1] == "2/2/2007"),]
+    if (nrow(data) > 0) {
+      all.data <- rbind(all.data, data)
+    }
+    if (has.header) {
+      has.header <- FALSE   #when you change the header to FALSE
+      rstart <- rstart + 1  #rstart needs to be increased by one
+    }
+    rstart = rstart + rblock
+  }
+  all.data
+}
+
+file = "household_power_consumption.txt"
+get.file(file, "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip")
+#data  <- read.file.by.blocks(file) #reading by blocks takes 70 seconds, but the next one takes 20 seconds!
+data0 <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE,
+                    na.strings="?", skip = 0, nrow = 2100000,
+                    colClasses = c(rep("character",2), rep("numeric",7)))
+
+Sys.setlocale("LC_TIME", "en_GB")
+data <- data0[(data0[,1] == "1/2/2007" | data0[,1] == "2/2/2007"),]
+data[, 2] <- as.POSIXct(strptime(paste(data[, 1], data[, 2]), "%d/%m/%Y %T"))
+data[, 3:9]  <- sapply(data[, 3:9], as.numeric)
+png(filename = "figure/plot3.png", width = 480, height = 480)
+with(data, plot(Time, Sub_metering_1, xlab = "", ylab = "Energy sub metering", type="n"))
+with(data, lines(Time, Sub_metering_1, col = "black"))
+with(data, lines(Time, Sub_metering_2, col = "red"))
+with(data, lines(Time, Sub_metering_3, col = "blue"))
+legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
